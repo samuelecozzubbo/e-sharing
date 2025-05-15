72fcfc01c7aaa5fbde727f47d91f7a4fe625333d
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2025-05-15 10:02:27 +0200

    project start

---
+++ src/main/java/com/e_sharing/E_sharing/ESharingApplication.java
@@ -0,0 +1,13 @@
+package com.e_sharing.E_sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_sharing/E_sharing/ESharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_sharing.E_sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
