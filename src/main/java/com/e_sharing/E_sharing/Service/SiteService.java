f51004b7f4b99ded007a9f9a1c6197b08de95ca4
Santo La Rocca <laroccasantoandrea@gmail.com>
2025-05-20 12:11:40 +0200

    fixed siteservice

--- src/main/java/com/e_sharing/E_sharing/Service/SiteService.java
+++ src/main/java/com/e_sharing/E_sharing/Service/SiteService.java
@@ -1,12 +1,15 @@
 package com.e_sharing.E_sharing.Service;
 
+import com.e_sharing.E_sharing.DTO.LeadDTO;
 import com.e_sharing.E_sharing.DTO.SiteDTO;
 import com.e_sharing.E_sharing.Model.Site;
 import com.e_sharing.E_sharing.Repositories.SiteRepository;
+import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -25,28 +28,35 @@ public class SiteService {
 
     //get all
     public List<SiteDTO> getAllSite(){
-        return ; //siteRepository.findAll();}
+        List<SiteDTO> sitesDTO = new ArrayList<>();
+        siteRepository.findAll().forEach(site -> sitesDTO.add(modelMapper.map(site, SiteDTO.class)));
+        return sitesDTO; //siteRepository.findAll();}
     }
 
     //get by id
-    public Optional<Site> getSiteById (Long id){return siteRepository.findById(id);}
+    public Optional<SiteDTO> getSiteById (Long id){
+        return siteRepository.findById(id).map(site -> modelMapper.map(site, SiteDTO.class));
+    }
 
     //salva un nuovo site controllando che non sia gia presente
-    public Site saveSite(SiteDTO site){
-        return siteRepository.save(modelMapper.map(site , Site.class));
+    @Transactional
+    public String saveSite(SiteDTO site){
+        Site newSite = modelMapper.map(site, Site.class);
+        siteRepository.save(newSite);
+        return "Site saved";
     }
 
     //delete by Id
     public void deleteSite(Long id){siteRepository.deleteById(id);}
 
     //update info
+    @Transactional
     public String result(Long id ,SiteDTO site){
         if (siteRepository.existsById(id)){
-            Site siteUpdate = modelMapper.map(site , Site.class);
             siteRepository.findById(id).map(existingSite -> {
-                existingSite.setName(siteUpdate.getName());
-                existingSite.setAddress(siteUpdate.getAddress());
-                existingSite.setCity(siteUpdate.getCity());
+                existingSite.setName(site.getName());
+                existingSite.setAddress(site.getAddress());
+                existingSite.setCity(site.getCity());
                 return siteRepository.save(existingSite);
             });
             return "Site updated";
